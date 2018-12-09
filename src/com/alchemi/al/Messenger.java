312a76cbd282bacdef8da9724a21fae99ffb2aa3
Pythonidae Julius Domitor <misha.smit@xs4all.nl>
2018-12-09 19:53:56 +0100

    Support for hovertext messages

--- src/com/alchemi/al/Messenger.java
+++ src/com/alchemi/al/Messenger.java
@@ -1,5 +1,6 @@
 package com.alchemi.al;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -9,6 +10,10 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class Messenger{
 
@@ -23,13 +28,13 @@ public class Messenger{
 	public String getMessage(String key) {
 		
 		String msg = this.fm.getConfig("messages.yml").getString(this.plugin.getDescription().getName() + "." + key);
-		return cc(msg);
+		return msg;
 		
 	}
 	
 	public String getTag() {
 		String msg = this.fm.getConfig("messages.yml").getString(this.plugin.getDescription().getName() + ".Tag");
-		return cc(msg);
+		return msg;
 	}
 	
 	public static String cc(String msg){
@@ -67,7 +72,7 @@ public class Messenger{
 		}
 		
 		for (Player r : Library.instance.getServer().getOnlinePlayers()) {
-			sendMsg(getTag() + " " + cc(msg), r);
+			sendMsg(cc(getTag() + " " + msg), r);
 		}
 //		Bukkit.broadcastMessage(getTag() + " " + cc(msg));
 		
@@ -83,6 +88,73 @@ public class Messenger{
 	
 	public static void sendMsg(String msg, CommandSender reciever, Map<String, String> vars) {
 		reciever.sendMessage(cc(parseVars(msg, vars)));
+	}
+	
+	public void broadcastHover(String mainText, String hoverText, Map<String, String> vars) {
+		if (mainText.contains("\n")) {
+			for (String msg : mainText.split("\n")) {
+				broadcastHover(msg, hoverText, vars);
+			}
+			return;
+		}
+		for (Player r : Library.instance.getServer().getOnlinePlayers()) {
+			sendHoverMsg(r, getTag() + " " + mainText, hoverText, vars);
+		}
+	}
+	
+	public void broadcastHover(String mainText, String hoverText) {
+		if (mainText.contains("\n")) {
+			for (String msg : mainText.split("\n")) {
+				broadcastHover(msg, hoverText);
+			}
+			return;
+		}
+		for (Player r : Library.instance.getServer().getOnlinePlayers()) {
+			sendHoverMsg(r, getTag() + " " + mainText, hoverText);
+		}
+	}
+	
+	public static void sendHoverMsg(Player reciever, String mainText, String hoverText, Map<String, String> vars) {
+		sendHoverMsg(reciever, parseVars(mainText, vars), parseVars(hoverText, vars));
+		
+	}
+	
+	public static void sendHoverMsg(Player reciever, String mainText, String hoverText) {
+		if (hoverText.substring(0, 1).equals("\n")) hoverText = hoverText.replaceFirst("\n", "");
+		
+//		String subText = mainText;
+		String mod = "&9";
+		String newText = "";
+		
+		System.out.println(mainText);
+		
+		for (String s : mainText.split(" ")) {
+			
+			if (s.contains("&")) {
+				int i = s.indexOf("&") + 1;
+				if (s.charAt(i) != ' ')	mod = "&" + s.charAt(i);
+			}
+			
+			if (mainText.split(" ")[0].equals(s)) newText = s;
+			else {
+				
+				if (s.charAt(0) == '&')	newText += " " + mod + s;
+				else newText += " " + s + mod;
+				
+			}
+			
+		}
+		
+		System.out.println(Arrays.asList(mainText.split(" ")));
+		System.out.println(newText);
+		
+		mainText = newText;
+		
+		TextComponent mainComponent = new TextComponent(cc(mainText));
+		mainComponent.setHoverEvent(new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(cc(hoverText)).create() ));
+		
+		reciever.spigot().sendMessage(mainComponent);
+		
 	}
 	
 }

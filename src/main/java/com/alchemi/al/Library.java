a6d388eb372bf6bcc5f2ebee36efa089cc53ce3b
Alchemi <misha.smit@xs4all.nl>
2019-05-23 23:17:18 +0200

    v1.8.3
    Changed startup message & fixed a bug

--- src/main/java/com/alchemi/al/Library.java
+++ src/main/java/com/alchemi/al/Library.java
@@ -5,8 +5,8 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.inventory.ItemStack;
 
 import com.alchemi.al.configurations.Messenger;
@@ -35,8 +35,19 @@ public class Library extends PluginBase implements Listener {
 		PersistentMeta.enable();
 		
 		Bukkit.getPluginManager().registerEvents(this, this);
+		 
+		for (String l : Messenger.cc("&2 __       __   ______   ______   ______   _______         ______  __    __        __       __  ________  _______   ______   ______  \r\n" + 
+				"&2/  \\     /  | /      \\ /      | /      \\ /       \\       /      |/  \\  /  |      /  \\     /  |/        |/       \\ /      | /      \\ \r\n" + 
+				"&200  \\   /10 |/111011  |111010/ /100111  |1010111  |      100110/ 10  \\ 10 |      01  \\   /10 |01101011/ 0110100  |001110/ /011101  |\r\n" + 
+				"&2101  \\ /000 |11 |__11 |  11 |  11 |  11 |10 |__00 |        01 |  011  \\10 |      010  \\ /000 |01 |__    01 |  11 |  00 |  10 |  01 |\r\n" + 
+				"&20100  /0101 |01    11 |  01 |  10 |  10 |01    11<         01 |  0011  11 |      0001  /1011 |01    |   10 |  11 |  11 |  01 |  00 |\r\n" + 
+				"&201 10 00/01 |00000100 |  01 |  11 |  00 |0111011  |        01 |  10 01 11 |      00 10 10/00 |00110/    10 |  10 |  01 |  00 |  10 |\r\n" + 
+				"&201 |000/ 00 |10 |  01 | _00 |_ 01 \\__11 |01 |  11 |       _00 |_ 10 |0011 |      11 |100/ 00 |00 |_____ 11 |__11 | _10 |_ 00 \\__11 |\r\n" + 
+				"&201 | 0/  11 |11 |  01 |/ 11   |00    01/ 00 |  01 |      / 11   |00 | 011 |      10 | 1/  11 |10       |01    01/ / 10   |11    01/ \r\n" + 
+				"&211/      00/ 10/   11/ 000010/  101111/  10/   01/       101000/ 01/   00/       00/      10/ 11011011/ 1111011/  001110/  011101/").split("\n")) {
+			Bukkit.getConsoleSender().sendMessage(l);
+		}
 		
-		getLogger().info(Messenger.cc("&8Hello Stonehenge!"));
 	}
 	
 	public void onDisable(){
@@ -134,6 +145,14 @@ public class Library extends PluginBase implements Listener {
 	public void onPlayerLogin(PlayerJoinEvent e) {
 		if (e.getPlayer().hasPermission("al.forcecheckupdate")) {
 			uc.check();
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerChat(AsyncPlayerChatEvent e) {
+		if (e.getPlayer().hasPermission("al.beingcool") 
+				&& e.getMessage().equalsIgnoreCase("hello there")) {
+			messenger.broadcast("&8&oGeneral Kenobi...", false);
 		}
 	}
 }

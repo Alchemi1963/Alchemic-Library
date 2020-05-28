f28aceab4d32970028469c2bca42ef4f6e682497
Alchemi <misha.smit@xs4all.nl>
2020-05-28 14:15:38 +0200

    Cleanup
    Removed NMS handling etc, was very messy and created more problems than it solved
    Removed traces of maven

---
+++ src/main/java/me/alchemi/al/Library.java
@@ -0,0 +1,264 @@
+package me.alchemi.al;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+
+import me.alchemi.al.configurations.Messenger;
+import me.alchemi.al.configurations.SexyConfiguration;
+import me.alchemi.al.database.mysql.MySQLDatabase;
+import me.alchemi.al.objects.base.PluginBase;
+import me.alchemi.al.objects.commands.PageCommands;
+import me.alchemi.al.objects.handling.CarbonDating;
+import me.alchemi.al.objects.handling.UpdateChecker;
+import me.alchemi.al.objects.meta.PersistentMeta;
+import me.alchemi.al.objects.placeholder.PapiParser;
+import me.alchemi.al.objects.placeholder.Parser;
+import me.alchemi.al.objects.placeholder.RegularParser;
+
+public class Library extends PluginBase implements Listener {
+	
+	private static Library instance;
+	
+	private static PersistentMeta meta;
+	
+	private static Parser parser;
+	
+	public SexyConfiguration sc;
+	
+	public UpdateChecker uc;
+	
+	@SuppressWarnings("serial")
+	private static final Map<Character, Integer> dictionary = new HashMap<Character, Integer>() {
+		{
+			put('a', 1);
+			put('b', 2);
+			put('c', 3);
+			put('d', 4);
+			put('e', 5);
+			put('f', 6);
+			put('g', 7);
+			put('h', 8);
+			put('i', 9);
+			put('j', 10);
+			put('k', 11);
+			put('l', 12);
+			put('m', 13);
+			put('n', 14);
+			put('o', 15);
+			put('p', 16);
+			put('q', 17);
+			put('r', 18);
+			put('s', 19);
+			put('t', 20);
+			put('u', 21);
+			put('v', 22);
+			put('w', 23);
+			put('x', 24);
+			put('y', 25);
+			put('z', 26);
+			put(' ', 27);
+			put('-', 28);
+			put('_', 29);
+			put('?', 30);
+			put('*', 31);
+		}
+	};
+	
+	@Override
+	public void onEnable() {
+		
+		MySQLDatabase.load();
+		
+		instance = this;
+		
+		this.SPIGOT_ID = 62777;
+		this.setMessenger(new Messenger(this));
+		
+		uc = new UpdateChecker(this);
+		
+		meta = new PersistentMeta();
+		
+		parser = getServer().getPluginManager().getPlugin("PlaceholderAPI") != null ? new PapiParser() : new RegularParser();
+		
+		Bukkit.getPluginManager().registerEvents(this, this);
+		Bukkit.getPluginManager().registerEvents(new PageCommands(), this);
+		
+		for (String l : Messenger.formatString("&211        00  101001  000110  101101  110011\r\n" + 
+				"&20011    0110  11  10    10    01  00  01  01\r\n" + 
+				"&201 00  01 00  001010    10    01  11  0010  \r\n" + 
+				"&200   01   01  01  00    01    01  01  00  10\r\n" + 
+				"&201        00  11  00  100010  111001  11  00\r\n\n" + 
+				"&2110000  00    11\r\n" + 
+				"&2  00    1011  10\r\n" + 
+				"&2  11    10 00 11\r\n" + 
+				"&2  00    10  1110\r\n" + 
+				"&2  00    01    10\r\n\n" + 
+				"&201        10  001000  0111    011001  001100\r\n" + 
+				"&21011    0101  00      10  11    00    00  01\r\n" + 
+				"&200 11  01 01  0000    01  01    00    11  01\r\n" + 
+				"&211   01   10  00      10  00    10    11  01\r\n" + 
+				"&201        10  010111  1111    001001  111010").split("\n")) {
+			Bukkit.getConsoleSender().sendMessage(l);
+		}
+	}
+	
+	public static Library getInstance() {
+		return instance;
+	}
+	
+	public void onDisable(){
+	
+		getLogger().info(Messenger.formatString("&6Saving player metadata..."));
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			PersistentMeta.save(player);
+		}
+		
+		getLogger().info(Messenger.formatString("&7I don't wanna go..."));
+	}
+	
+	/**
+	 * Gets an OfflinePlayer by name.
+	 * isn't deprecated, like {@linkplain Bukkit.getOfflinePlayer(String name)}
+	 * 
+	 * @param name 	The name of the player
+	 * @return		The OfflinePlayer
+	 */
+	public static OfflinePlayer getOfflinePlayer(String name) {
+		for (OfflinePlayer offP : Bukkit.getOfflinePlayers()) {
+			if (offP.getName().equals(name)) return offP;
+		}
+		return null;
+	}
+	
+	
+	public static Player getPlayer(String name) {
+		name = ChatColor.stripColor(Messenger.formatString(name));
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			if (player.getName().equals(name) || ChatColor.stripColor(Messenger.formatString(player.getDisplayName())).equals(name)) return player;
+		}
+		return null;
+	}
+	
+	/**
+	 * Give an itemstack to a player.
+	 * 
+	 * @param item		The itemstack to give.
+	 * @param player	The player to give it to.
+	 * @see {@link ItemStack}, {@link Player}
+	 */
+	public static void giveItemStack(ItemStack item, Player player) {
+		if (item.getAmount() > item.getMaxStackSize()) { 
+			ItemStack item2 = item.clone();
+			item2.setAmount(item.getAmount() - item.getMaxStackSize());
+			giveItemStack(item2, player);
+			item.setAmount(item.getMaxStackSize());
+		}
+		
+		if (player.getInventory().firstEmpty() == -1) {
+			player.getWorld().dropItem(player.getLocation(), item);
+		} else {
+			player.getPlayer().getInventory().addItem(item);
+		}
+	}
+	
+	/**
+	 * Test if a string is a number.
+	 * 
+	 * @param input the string to test.
+	 * @return Wether the string is a number or not.
+	 */
+	public static boolean testIfNumber(String input) {
+		Matcher m = Pattern.compile("\\d+(,|.)*").matcher(input);
+		
+		return m.matches();
+	}
+	
+	@EventHandler
+	public void onPlayerLogin(PlayerJoinEvent e) {
+		if (e.getPlayer().hasPermission("al.forcecheckupdate")) {
+			uc.check();
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerChat(AsyncPlayerChatEvent e) {
+		if ((e.getPlayer().hasPermission("al.beingcool") || e.getPlayer().isOp())
+				&& (e.getMessage().equalsIgnoreCase("hello there") || e.getMessage().toLowerCase().contains("hello there"))) {
+			Bukkit.getScheduler().scheduleSyncDelayedTask(instance, new Runnable() {
+				
+				@Override
+				public void run() {
+					messenger.broadcast("&7&oGeneral Kenobi...", false);
+					CarbonDating date = CarbonDating.getCurrentDateTime();
+					if (date.month.equals("11")
+							&& (date.day.equals("09") || date.day.equals("9"))) {
+						messenger.broadcast("&6&lHappy birthday, &2&l&oA&a&l&ol&2&l&oc&a&l&oh&2&l&oe&a&l&om&2&l&oi&6!", false);
+					}
+					
+				}
+			}, 5);
+		}
+	}
+	
+	public static String toBinary(String from) {
+		String output = "";
+		from = from.toLowerCase();
+		
+		for (int ch = 0; ch < from.length(); ch++) {
+			output += numToBinary(letterToNumber(from.charAt(ch)));
+		}
+		return output;
+	}
+	
+	public static int letterToNumber(char input) {
+		return dictionary.containsKey(input) ? dictionary.get(input) : -1;
+	}
+	
+	public static String numToBinary(int number) {
+		String bin = "";
+		
+		while (number > 0) {
+			for (int entry : Arrays.asList(4, 3, 2, 1, 0)) {
+				if (number - Math.pow(2, entry) >= 0) {
+					number -= Math.pow(2, entry);
+					bin += "1";
+					continue;
+				}
+				bin += "0";
+			}
+		}
+		
+		return bin;
+	}
+	
+	public static <T extends Comparable<? super T>> List<T> asSortedList(Collection<T> c) {
+	  List<T> list = new ArrayList<T>(c);
+	  if (!list.isEmpty()) Collections.sort(list);
+	  return list;
+	}
+	
+	public static PersistentMeta getMeta() {
+		return meta;
+	}
+	
+	public static Parser getParser() {
+		return parser;
+	}
+}
---
+++ src/main/java/me/alchemi/al/objects/base/ConfigBase.java
@@ -0,0 +1,213 @@
+package me.alchemi.al.objects.base;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.inventory.ItemStack;
+
+import me.alchemi.al.configurations.SexyConfiguration;
+import me.alchemi.al.objects.handling.ItemFactory;
+
+/**
+ * @author Alchemi
+ *
+ */
+public abstract class ConfigBase {
+
+	protected PluginBase plugin;
+	public ConfigBase instance;
+	
+	public interface IConfig{
+		
+		Object value();
+		
+		String key();
+		
+		SexyConfiguration getConfig();
+		
+		void get();
+		
+		default void set(Object value) throws IOException {
+			getConfig().set(key(), value);
+			getConfig().save();
+			get();
+		}
+		
+		default boolean asBoolean() {
+			return Boolean.valueOf(asString());
+		}
+		
+		default String asString() {
+			return String.valueOf(value());
+		}
+		
+		default Sound asSound() {
+			return Sound.valueOf(asString());
+		}
+		
+		@SuppressWarnings("unchecked")
+		default List<String> asStringList() {
+			if (value() instanceof List) {
+				if (((List<?>)value()).get(0) instanceof String
+						|| ((List<?>)value()).isEmpty()) {
+					return (List<String>)value();
+				}
+			}
+			return new ArrayList<String>();
+		}
+		
+		default int asInt() {
+			return Integer.valueOf(asString());
+		}
+		
+		default double asDouble() {
+			return Double.valueOf(asString());
+		}
+		
+		@SuppressWarnings("unchecked")
+		default List<Float> asFloatList() {
+			if (value() instanceof List) {
+				if (((List<?>)value()).get(0) instanceof Float
+						|| ((List<?>)value()).isEmpty()) {
+					return (List<Float>)value();
+				}
+			}
+			return new ArrayList<Float>();
+		}
+		
+		@SuppressWarnings("unchecked")
+		default List<Integer> asIntList() {
+			if (value() instanceof List) {
+				if (((List<?>)value()).get(0) instanceof Integer
+						|| ((List<?>)value()).isEmpty()) {
+					return (List<Integer>)value();
+				}
+			}
+			return new ArrayList<Integer>();
+		}
+		
+		default ItemStack asItemStack() {
+			if (value() instanceof ItemStack) {
+				return (ItemStack)value();
+			}
+			return new ItemFactory(Material.AIR);
+		}
+		
+		default Material asMaterial() {
+			return Material.getMaterial(asString());
+		}
+	}
+	
+	public interface IMessage {
+		
+		public void get();
+		
+		public String value();
+		
+		public String key();
+		
+		SexyConfiguration getConfig();
+	}
+	
+	public interface IConfigEnum{
+		
+		SexyConfiguration getConfig();
+		
+		File getFile();
+		
+		int getVersion();
+		
+	}
+	
+	public ConfigBase(PluginBase plugin) throws FileNotFoundException, IOException, InvalidConfigurationException {
+		instance = this;
+		this.plugin = plugin;
+		
+		for (IConfigEnum ce : getConfigs()) {
+			
+			SexyConfiguration file = ce.getConfig();
+			
+			int version = ce.getVersion();
+			
+			if(!file.getFile().exists()) {
+				plugin.saveResource(file.getFile().getName(), true);
+			}
+			
+			if(!file.isSet("File-Version-Do-Not-Edit") 
+					|| !file.get("File-Version-Do-Not-Edit").equals(version)) {
+				plugin.getMessenger().print("Your $file$ is outdated! Updating...".replace("$file$", file.getFile().getName()));
+				file.load();
+				file.update(SexyConfiguration.loadConfiguration(new InputStreamReader(plugin.getResource(file.getFile().getName()))));
+				file.set("File-Version-Do-Not-Edit", version);
+				file.save();
+				plugin.getMessenger().print("File successfully updated!");
+			}
+		}
+		
+		reload();
+	}
+	
+	public void reload() {
+		try {
+			if (getConfigs() != null) for (IConfigEnum c : getConfigs()) {
+				if (c == null) continue;
+				c.getConfig().load();	
+			}
+			
+			if (getEnums() != null) for (IConfig value : getEnums()) {
+				if (value == null) continue;
+				value.get();
+			}
+				
+			if (getMessages() != null) for (IMessage value : getMessages()) {
+				if (value == null) continue;
+				value.get();
+			}
+			
+		} catch (IOException | InvalidConfigurationException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void revertDefault() {
+		for (IConfigEnum c : getConfigs()) {
+			plugin.getMessenger().print("Reverting $file$ to default.".replace("$file$", c.getFile().getName()));
+			plugin.saveResource(c.getFile().getName(), true);
+		}
+		reload();
+	}
+	
+	public void save() {
+		
+		if (getEnums() != null) for (IConfig value : getEnums()) {
+			if (value == null) continue;
+			value.getConfig().set(value.key(), value.value());
+		}
+		
+		if (getMessages() != null) for (IMessage value : getMessages()) {
+			if (value == null) continue;
+			value.getConfig().set(value.key(), value.value());
+		}
+		
+		try {
+			if (getConfigs() != null) for (IConfigEnum c : getConfigs()) {
+				if (c == null) continue;
+				c.getConfig().save();
+			}
+		} catch (IOException e) {e.printStackTrace();}
+	}
+	
+	protected abstract IConfigEnum[] getConfigs();
+	
+	protected abstract Set<IConfig> getEnums();
+	
+	protected abstract Set<IMessage> getMessages();
+}
